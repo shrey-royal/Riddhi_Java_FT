f6df592d02e6b2f932b4cb3c61e70f8250235857
Shrey K <srkkadia@gmail.com>
2023-07-19 12:02:27 +0530

    switch

--- Basics/BoolToInt.java
+++ Basics/BoolToInt.java
@@ -1,7 +1,29 @@
 public class BoolToInt {
-    public static void main(String[] args) {
-        boolean b = true;
-        int i = b ? 1 : 0;  
-        System.out.println(i);
+    public static void main(String[] args) throws Exception {
+        // boolean b = true;
+        // int i = b ? 1 : 0;  
+        // System.out.println(i);
+
+        System.out.println("Enter any character: ");
+        char ch = (char) System.in.read();
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(ch + " is a vowel.");
+                break;
+        
+            default:
+                System.out.println(ch + " is a consonant.");
+                break;
+        }
     }
 }
