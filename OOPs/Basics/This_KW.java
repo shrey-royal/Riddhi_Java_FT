1e1a6cef7c0f5cbebd1b452cb66c9ccabfd07341
Shrey K <srkkadia@gmail.com>
2023-08-03 15:43:07 +0530

    this kwyword tasks

--- OOPs/Basics/This_KW.java
+++ OOPs/Basics/This_KW.java
@@ -43,6 +43,31 @@ public class This_KW {
         // amc = new AMC(1);
         // amc.print();
 
-        
+
     }
 }
+
+
+/*
+1. **Palindrome Checker:** Write a Java program to determine if a given string is a palindrome or not.
+
+2. **Prime Number Generator:** Develop a Java program that generates and prints all prime numbers within a given range.
+
+3. **Student Management System:** Create a Java class hierarchy to model a student management system, including classes for students, courses, and enrollment.
+
+4. **File Encryption and Decryption:** Implement a Java program that reads a file, encrypts its contents using a chosen encryption algorithm, and then decrypts it back to the original content.
+
+5. **Bank Account Management:** Design a Java program that simulates a simple bank account management system, allowing users to deposit, withdraw, and check their balances.
+
+6. **Online Shopping Cart:** Build a Java application for an online shopping cart, allowing users to add items, view the cart, and calculate the total cost.
+
+7. **Word Frequency Counter:** Write a Java program that reads a text file, counts the frequency of each word, and displays the top N most frequent words.
+
+8. **Employee Payroll System:** Develop a Java program that manages employee records, calculates their salaries based on different roles, and generates payroll reports.
+
+9. **Contact List Application:** Create a Java GUI application to manage a contact list, enabling users to add, edit, and delete contacts, as well as search for specific contacts.
+
+10. **Temperature Conversion Utility:** Design a Java program that converts temperatures between Celsius and Fahrenheit, allowing users to input values in one unit and see the corresponding value in the other.
+
+Feel free to adapt and expand upon these problem statements to create meaningful Java programming exercises.
+*/
