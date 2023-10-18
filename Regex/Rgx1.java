43581b95fe1aaff80a5f498fd4d476b4db113a1a
Shrey K <srkkadia@gmail.com>
2023-10-18 14:26:10 +0530

    Regex

--- Regex/Rgx1.java
+++ Regex/Rgx1.java
@@ -1,6 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Rgx1 {
     public static void main(String[] args) {
-        //
+        // Pattern p = Pattern.compile(".i");
+        // Matcher m = p.matcher("ri");
+        // boolean b = m.matches();
+        // System.out.println(b);
+
+        // boolean b2 = Pattern.compile(".s").matcher("as").matches();
+        // System.out.println(b2);
+
+        // boolean b3 = Pattern.matches(".s", "as");
+        // System.out.println(b3);
+
+        // System.out.println(Pattern.matches(".s", "as"));
+
+        //Character classes
+        // System.out.println(Pattern.matches("[amn]", "nma"));    //true (a or m or n comes one time)
+        // System.out.println(Pattern.matches("[amn]", "a"));
+        // System.out.println(Pattern.matches("[a-v]", "y"));
+
+
+        // System.out.println(Pattern.matches("[^abc]", "x")); //true (x is not a or b or c)
+        // System.out.println(Pattern.matches("[abc]?", "ab")); //false (among a or b or c)
+        // System.out.println(Pattern.matches("[abc]*", "aaaaa")); 
+
+        System.out.println(Pattern.matches("\\d", "abc"));
+        System.out.println(Pattern.matches("\\D{4}", "abcd"));
+        System.out.println();
+
+
     }
 }
 
@@ -39,9 +69,34 @@ Methods of Pattern Class:
 
 
 Regex Character Classes:
+1	[abc]	a, b, or c (simple class)
+2	[^abc]	Any character except a, b, or c (negation)
+3	[a-zA-Z]	a through z or A through Z, inclusive (range)
+4	[a-d[m-p]]	a through d, or m through p: [a-dm-p] (union)
+5	[a-z&&[def]]	d, e, or f (intersection)
+6	[a-z&&[^bc]]	a through z, except for b and c: [ad-z] (subtraction)
+7	[a-z&&[^m-p]]	a through z, and not m through p: [a-lq-z](subtraction)
+
+
 Regex Quantifiers:
+
+X?	X occurs once or not at all
+X+	X occurs once or more times
+X*	X occurs zero or more times
+X{n}	X occurs n times only
+X{n,}	X occurs n or more times
+X{y,z}	X occurs at least y times but less than z times
+
+
 Regex Meta Characters:
 
+.	Any character (may or may not match terminator)
+\d	Any digits, short of [0-9]
+\D	Any non-digit, short for [^0-9]
+\s	Any whitespace character, short for [\t\n\x0B\f\r]
+\S	Any non-whitespace character, short for [^\s]
+\w	Any word character, short for [a-zA-Z_0-9]
+\W	Any non-word character, short for [^\w]
 
 */
 
